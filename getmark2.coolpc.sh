800668405d560d373d09d2f5c7d5b163cd71854a
alastorid <alastorid@users.noreply.github.com>
2016-05-18 06:40:10 +0800

    quick fix for MSI GTX

--- getmark2.coolpc.sh
+++ getmark2.coolpc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PostData=`curl -sv http://coolpc.com.tw/3d.php|& grep -o "id=[0-9]\+ "|tr '\n' ','|sed 's/id=//g;s/ ,/,/g;s/^/N=/;s/,$//;s/$/\&S=1/;'`
 
-curl -sv --data "$PostData" "http://coolpc.com.tw/3dm.php" 2>/dev/null | iconv --from big5 --to utf8|grep -o "<td colspan=2 class=w>.*$"|grep kbd|sed 's/<[^>]*>/ /g;s/[^ a-zA-Z0-9$-]*//g;s/^ //;s/^\([^ ]* [^ ]* \)[^$]*\$/\1$/;s/[ ]\+/ /g;'|column -t
+curl -sv --data "$PostData" "http://coolpc.com.tw/3dm.php" 2>/dev/null | iconv --from big5 --to utf8|sed 's/GTX /GTX/g'|grep -o "<td colspan=2 class=w>.*$"|grep kbd|sed 's/<[^>]*>/ /g;s/[^ a-zA-Z0-9$-]*//g;s/^ //;s/^\([^ ]* [^ ]* \)[^$]*\$/\1$/;s/[ ]\+/ /g;'|column -t
 
 #curl -sv http://coolpc.com.tw/3d.php|& iconv --from big5 --to utf8| sed -n 's/^[^>]\+>\([^<]\+\)<.*$/\1/p'
