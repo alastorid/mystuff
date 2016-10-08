2dcefd272cb6999a920d7646a8e0546478a1ea95
alastorid <alastorid@users.noreply.github.com>
2016-10-09 03:15:10 +0800

    owo

--- tos/getMapInfo.sh
+++ tos/getMapInfo.sh
@@ -7,5 +7,5 @@ then
   mid=$1
 fi
 
-curl -sv "http://www.tosbase.com/game/world-map/$mid/" |& grep -o "<a href=\"database/monsters/[0-9][^<]*</a></span></div><div>[^<]*</div><div>[^<]*</div>"|sed 's/<a href="database\/monsters\/\([0-9]\+\)[^>]*>\([^<]*\)<\/a><\/span><\/div><div>Population: \([0-9]*\)<\/div><div>Respawn: \([0-9]*\)s<\/div>/id=\1_p=\3_r=\4_name="\2"/' |tr '\n' '\0' | xargs -0 -n1 -P0 ./getMonsterHPandEXP.sh | tr '_' ' '
+curl -sv "http://www.tosbase.com/game/world-map/$mid/" |& grep -o "<a href=\"database/monsters/[0-9][^<]*</a></span></div><div>[^<]*</div><div>[^<]*</div>"|sed 's/<a href="database\/monsters\/\([0-9]\+\)[^>]*>\([^<]*\)<\/a><\/span><\/div><div>Population: \([0-9]*\)<\/div><div>Respawn: \([0-9]*\)s<\/div>/id=\1_p=\3_r=\4_name="\2"/' |tr '\n' '\0' | xargs -0 -n1 -P0 ./getMonsterHPandEXP.sh | tr '_' ' ' |sort
 
