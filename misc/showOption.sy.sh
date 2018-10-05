11b0dea4d6f9777468ff86651fa073a0d8eb0d88
alastorid <alastorid@gmail.com>
2018-10-05 19:37:23 -0400

    0.0

--- misc/showOption.sy.sh
+++ misc/showOption.sy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -vs https://www.sinya.com.tw/diy/|& grep -o "option([0-9]\+,[^)]*)"|sed 's/^[^0-9]*\([^,]*\),.*/\1/'|sort -h
+curl -vs https://www.sinya.com.tw/diy/|& sed -n '/data-id="\([0-9]\+\)">/ {s/.*data-id="\([0-9]\+\)">/\1/;p;}'|sort -h
