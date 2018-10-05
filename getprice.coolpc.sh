afb76af3ddbdd5f9c2f3d8c718aeabfbf6db4e1d
alastorid <alastorid@gmail.com>
2018-10-05 19:30:04 -0400

    better

--- getprice.coolpc.sh
+++ getprice.coolpc.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-curl -vs http://coolpc.com.tw/evaluate.php|& iconv --from big5 --to utf8 -c |sed '/OPTION/!d;s/[^>]*>//;/OPTION value/d;/OPTGROUP/d;'
+
+raw_data=`curl -vs http://coolpc.com.tw/evaluate.php|& iconv --from big5 --to utf8 -c |sed '/OPTION/!d;s/[^>]*>//;/OPTION value/d;/OPTGROUP/d;'`
+
+if [ $1 == 1 ]; then
+	echo "$raw_data" | sed -n '{s/^\([^\$]*\), \(.*\)$/\2 , \1/; ;s/^\(\$[0-9]\+.\)\?\$\([0-9]\+\)[^,]*,/\$\2 ,/;p}'| sed  '/^[^\$]/d;/^\$1 ,/d; s/^\$//' | sort -h
+else
+	echo "$raw_data"
+fi
