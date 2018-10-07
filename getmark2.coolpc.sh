ebdfb3785c3ba3348c25c21fd2d4d12c31f533e3
alastorid <alastorid@gmail.com>
2018-10-06 23:43:27 -0400

    updated

--- getmark2.coolpc.sh
+++ getmark2.coolpc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-PostData=`curl -sv http://coolpc.com.tw/3d.php|& grep -o "id=[0-9]\+ "|tr '\n' ','|sed 's/id=//g;s/ ,/,/g;s/^/N=/;s/,$//;s/$/\&S=1/;'`
+m3dphp="http://www.coolpc.com.tw/dx.php"
+m3dlist="http://coolpc.com.tw/dx12.php"
 
-curl -sv --data "$PostData" "http://coolpc.com.tw/3dm.php" 2>/dev/null | iconv --from big5 --to utf8|sed 's/GTX /GTX/g'|grep -o "<td colspan=2 class=w>.*$"|grep kbd|sed 's/<[^>]*>/ /g;s/[^ a-zA-Z0-9$-]*//g;s/^ //;s/^\([^ ]* [^ ]* \)[^$]*\$/\1$/;s/[ ]\+/ /g;'|column -t
+PostData=`curl -sv $m3dlist|& grep -o "id=[0-9]\+ "|tr '\n' ','|sed 's/id=//g;s/ ,/,/g;s/^/N=/;s/,$//;s/$/\&S=1/;'`
+
+curl -sv --data "$PostData" "$m3dphp" 2>/dev/null | iconv --from big5 --to utf8|sed 's/<[^>]*>/,/g; s/,\{1,\}/,/g; s/^,\|,$//g; s/&#176;//g;s/, ,\$\([0-9]\+\),◆,★\(.*\)$/\2, \$\1/' |column -t -s ','
 
 #curl -sv http://coolpc.com.tw/3d.php|& iconv --from big5 --to utf8| sed -n 's/^[^>]\+>\([^<]\+\)<.*$/\1/p'
